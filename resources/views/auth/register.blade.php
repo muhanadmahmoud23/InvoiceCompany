cb89691d7ad8e2c66cc9c75b3cb1f64d471028ed
Muhanad Mahmoud <76536076+muhanadmahmoud23@users.noreply.github.com>
2021-01-11 09:07:33 +0200

    a

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.master2')
+@section('title')
+تسجيل دخول - برنامج الفواتير
+@stop
+
+@section('css')
+    <!-- Sidemenu-respoansive-tabs css -->
+    <link href="{{URL::asset('assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}" rel="stylesheet">
+@endsection
+@section('content')
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <!-- The image half -->
+            <!-- The content half -->
+            <div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+                <div class="login d-flex align-items-center py-2">
+                    <!-- Demo content-->
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
+                                <div class="card-sigin">
+                                    <div class="mb-5 d-flex"> <a href="{{ url('/' . $page='Home') }}"><img src="{{URL::asset('assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Muhanad<span>Mahmoud</span></h1></div>
+                                    <div class="card-sigin">
+                                        <h2>مرحبا بك</h2>
+                                        <h5 class="font-weight-semibold mb-4"> تسجيل حساب جديد</h5>
+                                        <form method="POST" action="{{ route('register') }}">
+                                            @csrf
+
+                                            <div class="form-group row">
+                                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('الاسم') }}</label>
+                                                <div class="col-md-6">
+                                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                                    @error('name')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+
+
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('البريد الالكترونى') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                                    @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('كلمة المرور') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                                    @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('تاكيد كلمة المرور ') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row mb-0">
+                                                <div class="col-md-12">
+                                                    <button type="submit" class="btn btn-main-primary  btn-block btn-main"  btn-block>
+                                                        {{ __('التسجيل') }}
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <br>
+                                        <button type="button" class="btn btn-main-primary btn-block"  onclick="location.href='{{ url('/')}}'"  >
+                                            تسجيل  الدخول
+                                       </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End -->
+                </div>
+            </div><!-- End -->
+
+            <div class="col-md-4 col-lg-8 col-xl-7 d-none d-md-flex bg-primary-transparent">
+                <div class="row wd-100p mx-auto text-center">
+                    <div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+                        <img src="{{URL::asset('assets/img/media/abstract-colorful-logo_23-2147504722.jpg')}}" class="my-auto ht-xl-200p wd-md-200p wd-xl-220p mx-auto" alt="logo">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
+@section('js')
+@endsection
